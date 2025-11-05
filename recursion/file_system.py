83f345ac22151e4d114cfda41cd2ba171f605fe6
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-11-05 13:28:09 +0330

    file system in a recursive manner

---
+++ recursion/file_system.py
@@ -0,0 +1,20 @@
+import os 
+
+total = 0
+def disk_usage(path: str) -> int:
+    global total
+    if os.path.exists(path):
+        total += os.path.getsize(path)
+
+        for filesnames in os.listdir(path):
+            if os.path.isdir(filesnames):
+                npath = os.path.join(path, filesnames)
+                total += disk_usage(npath)
+        return total
+    
+    else:
+        return "Not Valid Path" 
+    
+
+disk = disk_usage(r'C:\Users\AVA\Desktop\startbootstrap-clean-blog-gh-pages')
+print(disk)
